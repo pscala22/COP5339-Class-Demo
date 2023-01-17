c88cfd5acccf310fa18d9be633d5bf520394819b
Paul Scala <89755484+pscala22@users.noreply.github.com>
2023-01-17 19:34:36 +0000

    new DATE object with current time

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Date;
+  
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
+  // new DATE object with current time
+  Date dateNow = new Date();
+
+  Date dateCopy = (Date) dateNow.clone();
 }
